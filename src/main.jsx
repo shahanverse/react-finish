ea4f91f76298e3d3b562728ce09b5e2ef75c2883
shahan <shahanverse@gmail.com>
2026-01-14 13:29:17 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import CartProvider from "./context/CartProvider";
 import "./index.css";
 import AuthProvider from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <CartProvider>
         <App />
       </CartProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
